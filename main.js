9eb39ff9f99aec2d32da669b6deb5a35fbeb1ea3
Joseph Ogelenya <josephoges9@gmail.com>
2024-01-22 11:15:16 +0100

    Update main.js

--- main.js
+++ main.js
@@ -5,13 +5,13 @@ let prices = document.querySelectorAll(".card-container__card .price .txt");
 toggle.addEventListener("click", () => {
     if (toggle.checked) {
         // Switch to monthly
-        prices[0].innerHTML = "&dollar;19.99";
-        prices[1].innerHTML = "&dollar;24.99";
-        prices[2].innerHTML = "&dollar;39.99";
+        prices[0].innerHTML = "19.99";
+        prices[1].innerHTML = "24.99";
+        prices[2].innerHTML = "39.99";
     } else {
         // Switch back to yearly
-        prices[0].innerHTML = "&dollar;199.99";
-        prices[1].innerHTML = "&dollar;249.99";
-        prices[2].innerHTML = "&dollar;399.99";
+        prices[0].innerHTML = "199.99";
+        prices[1].innerHTML = "249.99";
+        prices[2].innerHTML = "399.99";
     }
 })
